0263da4076f93974bd35602a6b193108b56369f9
Maxim Moinat <maxim@thehyve.nl>
2016-11-08 17:54:52 +0100

    Added button for entering device name.

--- app/src/main/java/org/radarcns/empaticaE4/MainActivity.java
+++ app/src/main/java/org/radarcns/empaticaE4/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
@@ -15,8 +16,12 @@ import android.os.RemoteException;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -63,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
     private View[] mServerStatusIcons;
     private TextView[] mTemperatureLabels;
     private TextView[] mBatteryLabels;
+    private Button[] mDeviceInputButtons;
+    private String[] mInputDeviceNames = new String[4];
 
     /** Data formats **/
     final DecimalFormat singleDecimal = new DecimalFormat("0.0");
@@ -158,6 +165,13 @@ public class MainActivity extends AppCompatActivity {
                 (TextView) findViewById(R.id.batteryRow2),
                 (TextView) findViewById(R.id.batteryRow3),
                 (TextView) findViewById(R.id.batteryRow4)
+        };
+
+        mDeviceInputButtons = new Button[] {
+                (Button) findViewById(R.id.inputDeviceNameButtonRow1),
+                (Button) findViewById(R.id.inputDeviceNameButtonRow2),
+                (Button) findViewById(R.id.inputDeviceNameButtonRow3),
+                (Button) findViewById(R.id.inputDeviceNameButtonRow4)
         };
 
         uiRefreshRate = getResources().getInteger(R.integer.ui_refresh_rate);
@@ -543,5 +557,34 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void dialogInputDeviceName(final View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Device Serial Number:");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int row = getRowIndexFromView( v );
+                mInputDeviceNames[row] = input.getText().toString();
+                mDeviceInputButtons[row].setText( mInputDeviceNames[row] );
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
