701424ae2bbe219f1539800eea8e4a0e71ff54c5
Joris Borgdorff <github@blootsvoets.net>
2016-11-10 09:01:17 +0100

    Added comment in logger.info

--- app/src/main/java/org/radarcns/android/DeviceServiceConnection.java
+++ app/src/main/java/org/radarcns/android/DeviceServiceConnection.java
@@ -48,7 +48,7 @@ public class DeviceServiceConnection implements ServiceConnection {
             if (intent.getAction().equals(DEVICE_STATUS_CHANGED)) {
                 if (intent.hasExtra(DEVICE_STATUS_NAME)) {
                     deviceName = intent.getStringExtra(DEVICE_STATUS_NAME);
-                    logger.info(deviceName);
+                    logger.info("Device status changed of device {}", deviceName);
                 }
                 deviceStatus = DeviceStatusListener.Status.values()[intent.getIntExtra(DEVICE_STATUS_CHANGED, 0)];
                 logger.info("Updated device status to {}", deviceStatus);
