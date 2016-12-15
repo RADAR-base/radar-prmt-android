5a657fc3939e1c7eb885dfe41dfa67972f2dc1aa
Joris Borgdorff <joris@thehyve.nl>
2016-12-15 17:26:26 +0100

    Condensed DeviceTopics

--- app/src/main/java/org/radarcns/android/DeviceTopics.java
+++ app/src/main/java/org/radarcns/android/DeviceTopics.java
@@ -1,9 +1,34 @@
 package org.radarcns.android;
 
+import org.apache.avro.Schema;
 import org.apache.avro.specific.SpecificRecord;
 import org.radarcns.kafka.AvroTopic;
 import org.radarcns.key.MeasurementKey;
 
-public interface DeviceTopics {
-    AvroTopic<MeasurementKey, ? extends SpecificRecord> getTopic(String name);
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class DeviceTopics {
+    private final Map<String, AvroTopic<MeasurementKey, ? extends SpecificRecord>> topicMap;
+
+    protected DeviceTopics() {
+        topicMap = new HashMap<>();
+    }
+
+    protected <W extends SpecificRecord> AvroTopic<MeasurementKey, W> createTopic(String name,
+                                                        Schema valueSchema, Class<W> valueClass) {
+        AvroTopic<MeasurementKey, W> topic = new AvroTopic<>(
+                name, MeasurementKey.getClassSchema(), valueSchema,
+                MeasurementKey.class, valueClass);
+        topicMap.put(name, topic);
+        return topic;
+    }
+
+    public AvroTopic<MeasurementKey, ? extends SpecificRecord> getTopic(String name) {
+        AvroTopic<MeasurementKey, ? extends SpecificRecord> topic = topicMap.get(name);
+        if (topic == null) {
+            throw new IllegalArgumentException("Topic " + name + " unknown");
+        }
+        return topic;
+    }
 }
--- app/src/main/java/org/radarcns/empaticaE4/E4Topics.java
+++ app/src/main/java/org/radarcns/empaticaE4/E4Topics.java
@@ -1,16 +1,15 @@
 package org.radarcns.empaticaE4;
 
-import org.apache.avro.specific.SpecificRecord;
 import org.radarcns.android.DeviceTopics;
 import org.radarcns.kafka.AvroTopic;
 import org.radarcns.key.MeasurementKey;
 
 /** Topic manager for topics concerning the Empatica E4. */
-public class E4Topics implements DeviceTopics {
+public class E4Topics extends DeviceTopics {
     private final AvroTopic<MeasurementKey, EmpaticaE4Acceleration> accelerationTopic;
     private final AvroTopic<MeasurementKey, EmpaticaE4BatteryLevel> batteryLevelTopic;
     private final AvroTopic<MeasurementKey, EmpaticaE4BloodVolumePulse> bloodVolumePulseTopic;
-    private final AvroTopic<MeasurementKey, EmpaticaE4ElectroDermalActivity> electroDermalActivityTopic;
+    private final AvroTopic<MeasurementKey, EmpaticaE4ElectroDermalActivity> edaTopic;
     private final AvroTopic<MeasurementKey, EmpaticaE4InterBeatInterval> interBeatIntervalTopic;
     private final AvroTopic<MeasurementKey, EmpaticaE4Temperature> temperatureTopic;
     private final AvroTopic<MeasurementKey, EmpaticaE4SensorStatus> sensorStatusTopic;
@@ -28,35 +27,27 @@ public class E4Topics implements DeviceTopics {
     }
 
     private E4Topics() {
-        accelerationTopic = new AvroTopic<>("android_empatica_e4_acceleration", MeasurementKey.getClassSchema(), EmpaticaE4Acceleration.getClassSchema(), MeasurementKey.class, EmpaticaE4Acceleration.class);
-        batteryLevelTopic = new AvroTopic<>("android_empatica_e4_battery_level", MeasurementKey.getClassSchema(), EmpaticaE4BatteryLevel.getClassSchema(), MeasurementKey.class, EmpaticaE4BatteryLevel.class);
-        bloodVolumePulseTopic = new AvroTopic<>("android_empatica_e4_blood_volume_pulse", MeasurementKey.getClassSchema(), EmpaticaE4BloodVolumePulse.getClassSchema(), MeasurementKey.class, EmpaticaE4BloodVolumePulse.class);
-        electroDermalActivityTopic = new AvroTopic<>("android_empatica_e4_electrodermal_activity", MeasurementKey.getClassSchema(), EmpaticaE4ElectroDermalActivity.getClassSchema(), MeasurementKey.class, EmpaticaE4ElectroDermalActivity.class);
-        interBeatIntervalTopic = new AvroTopic<>("android_empatica_e4_inter_beat_interval", MeasurementKey.getClassSchema(), EmpaticaE4InterBeatInterval.getClassSchema(), MeasurementKey.class, EmpaticaE4InterBeatInterval.class);
-        temperatureTopic = new AvroTopic<>("android_empatica_e4_temperature", MeasurementKey.getClassSchema(), EmpaticaE4Temperature.getClassSchema(), MeasurementKey.class, EmpaticaE4Temperature.class);
-        sensorStatusTopic = new AvroTopic<>("android_empatica_e4_sensor_status", MeasurementKey.getClassSchema(), EmpaticaE4SensorStatus.getClassSchema(), MeasurementKey.class, EmpaticaE4SensorStatus.class);
-    }
-
-    @Override
-    public AvroTopic<MeasurementKey, ? extends SpecificRecord> getTopic(String name) {
-        switch (name) {
-            case "android_empatica_e4_acceleration":
-                return accelerationTopic;
-            case "android_empatica_e4_battery_level":
-                return batteryLevelTopic;
-            case "android_empatica_e4_blood_volume_pulse":
-                return bloodVolumePulseTopic;
-            case "android_empatica_e4_electrodermal_activity":
-                return electroDermalActivityTopic;
-            case "android_empatica_e4_inter_beat_interval":
-                return interBeatIntervalTopic;
-            case "android_empatica_e4_temperature":
-                return temperatureTopic;
-            case "android_empatica_e4_sensor_status":
-                return sensorStatusTopic;
-            default:
-                throw new IllegalArgumentException("Topic " + name + " unknown");
-        }
+        accelerationTopic = createTopic("android_empatica_e4_acceleration",
+                EmpaticaE4Acceleration.getClassSchema(),
+                EmpaticaE4Acceleration.class);
+        batteryLevelTopic = createTopic("android_empatica_e4_battery_level",
+                EmpaticaE4BatteryLevel.getClassSchema(),
+                EmpaticaE4BatteryLevel.class);
+        bloodVolumePulseTopic = createTopic("android_empatica_e4_blood_volume_pulse",
+                EmpaticaE4BloodVolumePulse.getClassSchema(),
+                EmpaticaE4BloodVolumePulse.class);
+        edaTopic = createTopic("android_empatica_e4_electrodermal_activity",
+                EmpaticaE4ElectroDermalActivity.getClassSchema(),
+                EmpaticaE4ElectroDermalActivity.class);
+        interBeatIntervalTopic = createTopic("android_empatica_e4_inter_beat_interval",
+                EmpaticaE4InterBeatInterval.getClassSchema(),
+                EmpaticaE4InterBeatInterval.class);
+        temperatureTopic = createTopic("android_empatica_e4_temperature",
+                EmpaticaE4Temperature.getClassSchema(),
+                EmpaticaE4Temperature.class);
+        sensorStatusTopic = createTopic("android_empatica_e4_sensor_status",
+                EmpaticaE4SensorStatus.getClassSchema(),
+                EmpaticaE4SensorStatus.class);
     }
 
     public AvroTopic<MeasurementKey, EmpaticaE4Acceleration> getAccelerationTopic() {
@@ -72,7 +63,7 @@ public class E4Topics implements DeviceTopics {
     }
 
     public AvroTopic<MeasurementKey, EmpaticaE4ElectroDermalActivity> getElectroDermalActivityTopic() {
-        return electroDermalActivityTopic;
+        return edaTopic;
     }
 
     public AvroTopic<MeasurementKey, EmpaticaE4InterBeatInterval> getInterBeatIntervalTopic() {
