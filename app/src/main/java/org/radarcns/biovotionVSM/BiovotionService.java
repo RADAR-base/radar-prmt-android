2c726d37c37d0934a0bbcd038a7b14977b585267
Sebastian Boettcher <seb.boett@gmail.com>
2017-02-21 14:06:12 +0100

    biovotion: added battery state schema, populated service and topics classes

--- app/src/main/java/org/radarcns/biovotionVSM/BiovotionService.java
+++ app/src/main/java/org/radarcns/biovotionVSM/BiovotionService.java
@@ -1,10 +1,70 @@
 package org.radarcns.biovotionVSM;
 
+import android.os.Bundle;
+
+import org.apache.avro.specific.SpecificRecord;
+import org.radarcns.RadarConfiguration;
+import org.radarcns.android.BaseDeviceState;
+import org.radarcns.android.DeviceManager;
 import org.radarcns.android.DeviceService;
+import org.radarcns.android.DeviceStatusListener;
+import org.radarcns.android.DeviceTopics;
+import org.radarcns.kafka.AvroTopic;
+import org.radarcns.key.MeasurementKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.radarcns.RadarConfiguration.DEFAULT_GROUP_ID_KEY;
 
 /**
  * A service that manages a BiovotionDeviceManager and a TableDataHandler to send store the data of a
  * Biovotion VSM and send it to a Kafka REST proxy.
  */
 public class BiovotionService extends DeviceService {
+    private static final Logger logger = LoggerFactory.getLogger(BiovotionService.class);
+    private BiovotionTopics topics;
+    private String groupId;
+
+    @Override
+    public void onCreate() {
+        logger.info("Creating Biovotion VSM service {}", this);
+        super.onCreate();
+
+        topics = BiovotionTopics.getInstance();
+    }
+
+    @Override
+    protected DeviceManager createDeviceManager() {
+        return new BiovotionDeviceManager(this, this, groupId, getDataHandler(), topics);
+    }
+
+    @Override
+    protected BaseDeviceState getDefaultState() {
+        BiovotionDeviceStatus newStatus = new BiovotionDeviceStatus();
+        newStatus.setStatus(DeviceStatusListener.Status.DISCONNECTED);
+        return newStatus;
+    }
+
+    @Override
+    protected DeviceTopics getTopics() {
+        return topics;
+    }
+
+    @Override
+    protected List<AvroTopic<MeasurementKey, ? extends SpecificRecord>> getCachedTopics() {
+        return Arrays.<AvroTopic<MeasurementKey, ? extends SpecificRecord>>asList(
+                topics.getBatteryStateTopic(), topics.getBloodPulseWaveTopic(),
+                topics.getHeartRateTopic());
+    }
+
+    @Override
+    protected void onInvocation(Bundle bundle) {
+        super.onInvocation(bundle);
+        if (groupId == null) {
+            groupId = RadarConfiguration.getStringExtra(bundle, DEFAULT_GROUP_ID_KEY);
+        }
+    }
 }
--- app/src/main/java/org/radarcns/biovotionVSM/BiovotionTopics.java
+++ app/src/main/java/org/radarcns/biovotionVSM/BiovotionTopics.java
@@ -1,7 +1,51 @@
 package org.radarcns.biovotionVSM;
 
 import org.radarcns.android.DeviceTopics;
+import org.radarcns.biovotion.BiovotionVSMBatteryState;
+import org.radarcns.biovotion.BiovotionVSMBloodPulseWave;
+import org.radarcns.biovotion.BiovotionVSMHeartRate;
+import org.radarcns.kafka.AvroTopic;
+import org.radarcns.key.MeasurementKey;
 
 /** Topic manager for topics concerning the Biovotion VSM. */
 public class BiovotionTopics extends DeviceTopics {
+    private final AvroTopic<MeasurementKey, BiovotionVSMBatteryState> batteryStateTopic;
+    private final AvroTopic<MeasurementKey, BiovotionVSMBloodPulseWave> bloodPulseWaveTopic;
+    private final AvroTopic<MeasurementKey, BiovotionVSMHeartRate> heartRateTopic;
+
+    private static final Object syncObject = new Object();
+    private static BiovotionTopics instance = null;
+
+    public static BiovotionTopics getInstance() {
+        synchronized (syncObject) {
+            if (instance == null) {
+                instance = new BiovotionTopics();
+            }
+            return instance;
+        }
+    }
+
+    private BiovotionTopics() {
+        batteryStateTopic = createTopic("android_biovotion_battery_state",
+                BiovotionVSMBatteryState.getClassSchema(),
+                BiovotionVSMBatteryState.class);
+        bloodPulseWaveTopic = createTopic("android_biovotion_blood_pulse_wave",
+                BiovotionVSMBloodPulseWave.getClassSchema(),
+                BiovotionVSMBloodPulseWave.class);
+        heartRateTopic = createTopic("android_biovotion_heart_rate",
+                BiovotionVSMHeartRate.getClassSchema(),
+                BiovotionVSMHeartRate.class);
+    }
+
+    public AvroTopic<MeasurementKey, BiovotionVSMBatteryState> getBatteryStateTopic() {
+        return batteryStateTopic;
+    }
+
+    public AvroTopic<MeasurementKey, BiovotionVSMBloodPulseWave> getBloodPulseWaveTopic() {
+        return bloodPulseWaveTopic;
+    }
+
+    public AvroTopic<MeasurementKey, BiovotionVSMHeartRate> getHeartRateTopic() {
+        return heartRateTopic;
+    }
 }
