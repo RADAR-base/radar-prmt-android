afac6df7bf10be43a83d86abb87cca0a067d794d
this-Aditya <adityamishra2217@gmail.com>
2024-09-12 18:52:07 +0530

    Configuring oAuthManager in AuthService

--- app/src/main/java/org/radarcns/detail/AuthServiceImpl.kt
+++ app/src/main/java/org/radarcns/detail/AuthServiceImpl.kt
@@ -3,11 +3,13 @@ package org.radarcns.detail
 import org.radarbase.android.auth.AppAuthState
 import org.radarbase.android.auth.AuthService
 import org.radarbase.android.auth.LoginManager
+import org.radarbase.android.auth.oauth2.OAuth2LoginManager
 import org.radarbase.android.auth.portal.ManagementPortalLoginManager
 
 class AuthServiceImpl : AuthService() {
     override fun createLoginManagers(appAuth: AppAuthState): List<LoginManager> = listOf(
         ManagementPortalLoginManager(this, appAuth),
+        OAuth2LoginManager(this)
     )
 
     override fun showLoginNotification() = Unit
