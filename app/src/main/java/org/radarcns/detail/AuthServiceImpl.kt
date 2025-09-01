5283f5289c1f9e792d9f9e7dd920e8b12bdec0ba
this-Aditya <adityamishra2217@gmail.com>
2025-09-01 08:26:08 +0530

    SepLoginManager added to auth service's managers list

--- app/src/main/java/org/radarcns/detail/AuthServiceImpl.kt
+++ app/src/main/java/org/radarcns/detail/AuthServiceImpl.kt
@@ -4,10 +4,12 @@ import org.radarbase.android.auth.AppAuthState
 import org.radarbase.android.auth.AuthService
 import org.radarbase.android.auth.LoginManager
 import org.radarbase.android.auth.portal.ManagementPortalLoginManager
+import org.radarbase.android.auth.sep.SEPLoginManager
 
 class AuthServiceImpl : AuthService() {
     override fun createLoginManagers(appAuth: AppAuthState): List<LoginManager> = listOf(
         ManagementPortalLoginManager(this, appAuth),
+        SEPLoginManager(this, appAuth)
     )
 
     override fun showLoginNotification() = Unit
