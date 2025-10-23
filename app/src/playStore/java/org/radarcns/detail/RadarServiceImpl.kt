4bb9bd2f16eda05e91a5e62ea88e3bc74e1619dd
this-Aditya <adityamishra2217@gmail.com>
2025-10-23 20:03:20 +0530

    Removing the empatica plugin

--- app/src/playStore/java/org/radarcns/detail/RadarServiceImpl.kt
+++ app/src/playStore/java/org/radarcns/detail/RadarServiceImpl.kt
@@ -26,7 +26,6 @@ import org.radarbase.android.source.SourceProvider
 import org.radarbase.monitor.application.ApplicationStatusProvider
 import org.radarbase.passive.audio.OpenSmileAudioProvider
 import org.radarbase.passive.bittium.FarosProvider
-import org.radarbase.passive.empatica.E4Provider
 import org.radarbase.passive.google.activity.GoogleActivityProvider
 import org.radarbase.passive.google.places.GooglePlacesProvider
 import org.radarbase.passive.google.sleep.GoogleSleepProvider
@@ -43,7 +42,6 @@ class RadarServiceImpl : RadarService() {
     override val plugins: List<SourceProvider<*>> = listOf(
         ApplicationStatusProvider(this),
         OpenSmileAudioProvider(this),
-        E4Provider(this),
         FarosProvider(this),
 //        PolarProvider(this),
         PhoneBluetoothProvider(this),
